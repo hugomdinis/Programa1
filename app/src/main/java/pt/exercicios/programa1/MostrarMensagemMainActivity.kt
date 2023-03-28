2288e48ec80ef78618ce9bd725ea4d43ec73bdaa
hugomdinis <hugomdinis@hotmail.com>
2023-03-28 10:00:25 +0100

    Adicionar atividade para mostrar as mensagens

---
+++ app/src/main/java/pt/exercicios/programa1/MostrarMensagemMainActivity.kt
@@ -0,0 +1,11 @@
+package pt.exercicios.programa1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MostrarMensagemMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mostrar_mensagem_main)
+    }
+}
