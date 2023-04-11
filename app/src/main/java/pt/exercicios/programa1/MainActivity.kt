5a6375429b6d8427d9c841e23c1aaa7f3c1af4cc
hugomdinis <hugomdinis@hotmail.com>
2023-04-11 09:22:36 +0100

    Chamar a função enviaMensagem mediante o clique do botão enviar

--- app/src/main/java/pt/exercicios/programa1/MainActivity.kt
+++ app/src/main/java/pt/exercicios/programa1/MainActivity.kt
@@ -2,10 +2,19 @@ package pt.exercicios.programa1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonEnviar = findViewById<Button>(R.id.buttonEnviar)
+        buttonEnviar.setOnClickListener { enviaMensagem() }
+    }
+
+    private fun enviaMensagem() {
+        Toast.makeText(this, "enviar mensagem", Toast.LENGTH_SHORT).show()
     }
 }
